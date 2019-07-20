958f9a21bb4535ea38708292333f0cdbc811f84d
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:10:57 +0900

    published

--- js/isArray.js
+++ js/isArray.js
@@ -2,22 +2,18 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
-		
-		var _toString = Object.prototype.toString;
+(function() {
+	'use strict';
+	
+	var _toString = Object.prototype.toString;
 
-		/**
-		 * @name isArray
-		 * @since 2017-12-06
-		 * @param {*} value
-		 * @return {boolean}
-		 */
-		window.isArray = function(value) {
-			return _toString.call(value) === '[object Array]';
-		};
-	})();
-}catch(e) {
-	console.error(e);
-}
+	/**
+	 * @name isArray
+	 * @since 2017-12-06
+	 * @param {*} value
+	 * @return {boolean}
+	 */
+	window.isArray = function(value) {
+		return _toString.call(value) === '[object Array]';
+	};
+})();
